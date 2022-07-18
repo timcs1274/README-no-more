e173906ca749e2182b482ba60d9b0cbcd880a3ac
timcs <timcs1274@gmail.com>
2022-07-18 19:38:02 -0400

    added tbl of contents to markdown

--- assets/index.js
+++ assets/index.js
@@ -7,49 +7,90 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'Please enter your desired title.'
+        message: 'Please enter your desired title.',
+        default: 'Title',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Please enter a description of the repository.'
+        message: 'Please enter a description of the repository.',
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Instruct users on how the installation process.'
+        message: 'Instruct users on how the installation process.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please include any tests that you may have.',
     },
     {
         type: 'input',
         name: 'contributing',
-        message: 'Please include information on how to contribute'
+        message: 'Please include information on how to contribute',
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'Please include any tests that you may have.'
+        message: 'Please include any tests that you may have.',
     },
 ];
 
 //Template literal string for displaying/formatting the info
-function generateMarkdown(data) {
-    return `#${data.title}
-    ##${data.description}
-    ##${data.installation}
-    ##${data.contributing}
-    ##${data.tests}
-    `;
+function generateMarkdown(dataUser) {
+    //Create table of contents based on the users responses
+    let tblOfContents = `## Table of Contents`;
+  
+    if (dataUser.installation !== '') { tblOfContents += `
+    * [Installation](#installation)` };
+  
+    if (dataUser.usage !== '') { tblOfContents += `
+    * [Usage](#usage)` };
+  
+    if (dataUser.contributing !== '') { tblOfContents += `
+    * [Contributing](#contributing)` };
+  
+    if (dataUser.tests !== '') { tblOfContents += `
+    * [Tests](#tests)` };
+
+    // Title and description of markdown
+    let titleMarkdown = 
+    `# ${dataUser.title}
+    
+    ## Description
+    *Include a description here:*
+
+    ${dataUser.description}
+    `
+
+    //Inputing the table of contents
+    titleMarkdown += tblOfContents
+
+    //Inputing the license section
+    titleMarkdown +=`
+    * [License](#license)`;
+
+    //Inputing the installation section
+    if (dataUser.installation !== '') {
+        titleMarkdown +=
+        `
+        ## Installation
+        *Include installation steps here:
+        ${dataUser.installation}`
+    };
+    
 }
 
 //Function to create the file
-const fileName = 'README.md';
+const fileName = 'ExampleREADME.md';
 function writeFile(fileName, data) {
     const readmeFile = generateMarkdown(data);
     fs.writeFile(fileName, readmeFile, err => {
         if(err) {
             console.log(err)
         }
-        console.log('README.md Created.')
+        console.log('ExampleREADME.md Created.')
     })
 }
 
