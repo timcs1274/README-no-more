b20f2e6621e219c200fd6f313ff679e495d23083
timcs <timcs1274@gmail.com>
2022-07-18 12:53:53 -0400

    added template literal function

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,14 @@
+//Packages needed to install
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//Array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter your desired title.'
+    },
     {
         type: 'input',
         name: 'title',
@@ -9,3 +16,8 @@ const questions = [
     },
 ];
 
+//Template literal string for displaying/formatting the info
+function generateMarkdown(data) {
+    return `##${data.title}
+    `
+}
