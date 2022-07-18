d65515c0d710de6c280751c18222916433eacca7
timcs <timcs1274@gmail.com>
2022-07-18 18:13:43 -0400

    added readme inputs

--- assets/index.js
+++ assets/index.js
@@ -11,13 +11,34 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'title',
-        message: 'Please enter your desired title.'
+        name: 'description',
+        message: 'Please enter a description of the repository.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Instruct users on how the installation process.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please include information on how to contribute'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please include any tests that you may have.'
     },
 ];
 
 //Template literal string for displaying/formatting the info
 function generateMarkdown(data) {
-    return `##${data.title}
+    return `#${data.title}
+    ##${data.description}
+    ##${data.installation}
+    ##${data.contributing}
+    ##${data.tests}
     `
 }
+
+//Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
