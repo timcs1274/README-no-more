edaa46b0009161d24a6d000f8217606ca4d488b2
timcs <timcs1274@gmail.com>
2022-07-18 20:10:04 -0400

    added export for markdown generation

--- assets/generatemarkdown.js
+++ assets/generatemarkdown.js
@@ -1,42 +1,3 @@
-//Packages needed to install
-const fs = require('fs');
-const inquirer = require('inquirer');
-
-//Array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Please enter your desired title.',
-        default: 'Title',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter a description of the repository.',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Instruct users on how the installation process.',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please include any tests that you may have.',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please include information on how to contribute',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please include any tests that you may have.',
-    },
-];
-
 //Template literal string for displaying/formatting the info
 function generateMarkdown(dataUser, userInput) {
     //Create table of contents based on the users responses
@@ -124,3 +85,4 @@ function generateMarkdown(dataUser, userInput) {
     return titleMarkdown;
     
 }
+module.exports = generateMarkdown;
